1e29ac619f2a0196947c72130d92b4cf663b4d01
atacan <atacandur@icloud.com>
2024-09-01 12:12:35 +0200

    Initial Commit

---
+++ DiscussionShareEnumState/DiscussionShareEnumStateApp.swift
@@ -0,0 +1,15 @@
+//
+// https://github.com/atacan
+// 01.09.24
+	
+
+import SwiftUI
+
+@main
+struct DiscussionShareEnumStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
